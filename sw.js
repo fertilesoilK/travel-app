4554e5b022a4a9b5f84f76f266edcf42b39800d8
fertilesoilK <fertilesoil.kuki@gmail.com>
2026-08-18 01:57:15 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,33 @@
+const CACHE_NAME = 'travel-app-v1';
+const urlsToCache = [
+    './',
+    './index.html',
+    './style.css',
+    './main.js',
+    './schedule.js',
+    './budget.js',
+    './expense.js',
+    './todo.js',
+    './dashboard.js'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            })
+    );
+});
